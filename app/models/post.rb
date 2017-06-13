ff2319b634592e353a2506244a99b7f1f297b657
yungpanko <jrjohns@mit.edu>
2017-06-13 10:15:19 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
+class ClickBaitValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /won't believe|secret|top \d|guess/i
+      record.errors[attribute] << (options[:message] || "is not an clickbait-y enough.")
+    end
+  end
+end
+
 class Post < ActiveRecord::Base
+  validates :title, presence: true, click_bait: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
 end
